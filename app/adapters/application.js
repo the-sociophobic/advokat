be4ed2b5374662ca19c765d9121e443e40430fda
the-sociophobic <torikowashi@gmail.com>
2017-06-09 00:52:36 +0300

    simple auth (noth works)

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'http://agentstvo-adv.ru:27017/test'
+  host: 'http://agentstvo-adv.ru:8080/test',
+  headers: function() {
+    return { Authorization: 'Basic YWRtaW46ZGVybW8zMjE=' };
+  }
 });
--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+  errorMessage: 'loool ne rabptait',
+
   queryParams: ['category'],
   category: null,
-  actions: {
+  
+  activate: function() {
+    this.get('session').authenticate('authenticator:oauth2', 'admin', 'dermo321').catch((reason) => {
+        this.set('errorMessage', reason.error || reason);
+    });
+  },
+  
+  actions:  {
     filterByCity(param) {
       if (param !== '') {
         return this.get('store').query('rental', { city: param });
