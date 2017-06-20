eecfae05c9818603fb85bf5cf9898ae563e89bc1
the-sociophobic <torikowashi@gmail.com>
2017-06-21 01:32:39 +0300

    melochi

---
+++ app/components/custom-dropdown.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  isExpanded: false,
+  
+  actions: {
+    toggleExpand() {
+      this.toggleProperty('isExpanded');
+    },
+    Expand() {
+      this.set('isExpanded', true);
+    },
+    Collapse() {
+      this.set('isExpanded', false);
+    },
+    toggleLabel(label) {
+      var hehe = this.get('checked').copy();
+      var index = hehe.indexOf(label);
+      
+      if (index > -1) {
+        hehe.splice(index, 1);
+      }
+      else {
+        hehe.push(label);
+      }
+      this.set('checked', hehe);
+      
+      this.get('commit')();
+    },
+    any() {
+      this.set('checked', 'Любой');
+      this.set('isExpanded', false);
+    }
+  }
+});
